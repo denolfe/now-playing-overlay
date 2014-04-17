99899e97b247e5beeeb00c9c55df21dc5c0b25b5
Elliot DeNolf <denolfe@gmail.com>
2014-04-17 08:47:15 -0400

    Basic Express setup

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ watch('./NowPlaying.txt', function(file) {
   search.getInfo();
 
 });
+
+var express = require('express')
+  , app = express()
+  , server = require('http').createServer(app)
+  , bodyParser = require('body-parser');
+
+var port = 1337;
+server.listen(port);
+console.log('Server started at localhost:' + port)
+
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser({strict: false}));
+app.set('views', './views');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
