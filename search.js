6ebf45181ff46a97ac0455355f223cc720968ad9
Elliot DeNolf <denolfe@gmail.com>
2014-04-21 16:44:14 -0400

    JS Beautify formatting

--- search.js
+++ search.js
@@ -1,4 +1,4 @@
-exports.getArt = function (query) {
+exports.getArt = function(query) {
   /**
    * Example that executes a Spotify "Search" and parses the XML results using
    * node-xml2js.
@@ -12,15 +12,15 @@ exports.getArt = function (query) {
   var username = process.env.SPOTIFY_USERNAME;
   var password = process.env.SPOTIFY_PASSWORD;
 
-  Spotify.login(username, password, function (err, spotify) {
+  Spotify.login(username, password, function(err, spotify) {
     if (err) throw err;
 
-    spotify.search(query, function (err, xml) {
+    spotify.search(query, function(err, xml) {
       if (err) throw err;
       spotify.disconnect();
 
       var parser = new xml2js.Parser();
-      parser.on('end', function (data) {
+      parser.on('end', function(data) {
         var album_uri;
         var open = require('open');
         try {
@@ -29,11 +29,10 @@ exports.getArt = function (query) {
           console.log(spotify.sourceUrls.small + album_uri);
           // open(spotify.sourceUrls.small + album_uri);
 
-          download(spotify.sourceUrls.small + album_uri, 'public/img/Cover.png', function(){
+          download(spotify.sourceUrls.small + album_uri, 'public/img/Cover.png', function() {
             console.log('Art downloaded successfully.');
           });
-        }
-        catch (ignore) {
+        } catch (ignore) {
           console.log("Album art not found. Checking song art...");
           try {
             // Check Song for Art
@@ -41,20 +40,18 @@ exports.getArt = function (query) {
             console.log(spotify.sourceUrls.small + album_uri);
             // open(spotify.sourceUrls.small + album_uri);
 
-            download(spotify.sourceUrls.small + album_uri, 'public/img/Cover.png', function(){
+            download(spotify.sourceUrls.small + album_uri, 'public/img/Cover.png', function() {
               console.log('Art downloaded successfully.');
             });
-          }
-          catch (ignore2) {
+          } catch (ignore2) {
             console.log("Can't find any art... Using unknown cover art.");
             try {
               fs.writeFileSync('public/img/Cover.png', fs.readFileSync('public/img/no-cover.png'));
-            }
-            catch (err) {
+            } catch (err) {
               console.log(err);
             }
           }
-          
+
         }
       });
       parser.parseString(xml);
@@ -64,44 +61,38 @@ exports.getArt = function (query) {
 }
 
 var lastCommand;
-exports.getNowPlaying = function () {
-    var out = {};
-    var path = require('path');
-    var filePath = path.join(__dirname + '/public/NowPlaying.txt');
+exports.getNowPlaying = function() {
+  var out = {};
+  var path = require('path');
+  var filePath = path.join(__dirname + '/public/NowPlaying.txt');
 
-    var nowPlaying = fs.readFileSync(filePath,'utf8').replace(/^\s+|\s+$/g, '');
-    console.log('Last Command: ' + lastCommand);
-    if (nowPlaying == '')
-    {
-      console.log('Music is paused.');
-      out.command = 'pauseSong';
-      out.nowPlaying = '';
+  var nowPlaying = fs.readFileSync(filePath, 'utf8').replace(/^\s+|\s+$/g, '');
+  console.log('Last Command: ' + lastCommand);
+  if (nowPlaying == '') {
+    console.log('Music is paused.');
+    out.command = 'pauseSong';
+    out.nowPlaying = '';
+  } else {
+    console.log('Now Playing: ' + nowPlaying);
+    if (lastCommand == 'pauseSong') {
+      out.command = 'startSong'
+    } else {
+      out.command = 'nextSong';
     }
-    else
-    {
-      console.log('Now Playing: ' + nowPlaying); 
-      if (lastCommand == 'pauseSong')
-      {
-        out.command = 'startSong'
-      }
-      else
-      {
-        out.command = 'nextSong';
-      }
-      out.nowPlaying = nowPlaying;  
-      this.getArt(nowPlaying);  
-    }
-    lastCommand = out.command;
-    return out;
+    out.nowPlaying = nowPlaying;
+    this.getArt(nowPlaying);
   }
+  lastCommand = out.command;
+  return out;
+}
 
 
 // http://stackoverflow.com/questions/12828187/saving-an-image-file-with-node-js-request-library-causes-exception
 var fs = require('fs'),
-request = require('request');
+  request = require('request');
 
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
+var download = function(uri, filename, callback) {
+  request.head(uri, function(err, res, body) {
     console.log('content-type:', res.headers['content-type']);
     console.log('content-length:', res.headers['content-length']);
 
@@ -111,28 +102,29 @@ var download = function(uri, filename, callback){
 
 
 function getInfo() {
-  var LastfmAPI = require ('lastfmAPI');
+  var LastfmAPI = require('lastfmAPI');
 
   var lfm = new LastfmAPI({
-      'api_key' : process.env.LASTFM_API_KEY,
-      'secret' : process.env.LASTFM_SECRET
+    'api_key': process.env.LASTFM_API_KEY,
+    'secret': process.env.LASTFM_SECRET
   });
 
   var artist = "";
   var track = "";
   lfm.user.getRecentTracks({
     'user': process.env.LASTFM_USERNAME
-  }, function (err, track) {
-      if (err) { 
-        console.log(err);
-        throw err; }
-      // console.log(track.track[0]);
-      artist = track.track[0].artist["#text"];
-      track = track.track[0].name;
-      // console.log(artist);
-      // console.log(track);
-      // console.log(track.track[0].image[3]["#text"])
-      getArt(artist, track);
+  }, function(err, track) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    // console.log(track.track[0]);
+    artist = track.track[0].artist["#text"];
+    track = track.track[0].name;
+    // console.log(artist);
+    // console.log(track);
+    // console.log(track.track[0].image[3]["#text"])
+    getArt(artist, track);
   });
 
 }
--- server.js
+++ server.js
@@ -6,18 +6,20 @@ var search = require('./search.js');
 
 
 
-var express = require('express')
-  , app = express()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server)
-  , bodyParser = require('body-parser');
+var express = require('express'),
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server),
+  bodyParser = require('body-parser');
 
 var port = 1337;
 server.listen(port);
 console.log('Server started at localhost:' + port + '...');
 
-app.use(express.static(__dirname+'/public'));
-app.use(bodyParser({strict: false}));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser({
+  strict: false
+}));
 app.set('views', './views');
 app.engine('html', require('ejs').renderFile);
 
@@ -26,29 +28,29 @@ app.get('/', function(req, res) {
 });
 
 var socket;
-io.sockets.on('connection', function (sock) {
-    socket = sock;
-    stateChange();
+io.sockets.on('connection', function(sock) {
+  socket = sock;
+  stateChange();
 });
 
-if (process.platform == 'win32')
-{
+if (process.platform == 'win32') {
   console.log('Starting Helper script...');
-  winspawn('helper.ahk');  
-}
-else
-{
+  winspawn('helper.ahk');
+} else {
   console.log('Helper script can only run on Windows machines.');
 }
 
 
-function stateChange () {
+function stateChange() {
   console.log('\nSong State Change.');
   var np = search.getNowPlaying();
   if (typeof socket === 'undefined')
     console.log('No page currently listening...');
   else
-    socket.emit('message', {'command': np.command, 'info': np});  
+    socket.emit('message', {
+      'command': np.command,
+      'info': np
+    });
 }
 
 watch('./public/NowPlaying.txt', stateChange);
